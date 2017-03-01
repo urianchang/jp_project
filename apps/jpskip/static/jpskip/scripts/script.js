174b6b1a06c8195018210cc47bfcf1691003ad2b
Arnold Son <hyomin.son@alum.utoronto.ca>
2017-02-28 23:45:25 -0800

    minor changes to templates step4.html and step5.html, updated script.js to make step5 functional

--- apps/jpskip/static/jpskip/scripts/script.js
+++ apps/jpskip/static/jpskip/scripts/script.js
@@ -7,3 +7,13 @@ $('#test').submit(function(event){
     $("span").text("WRONG!").show().fadeOut(3000)
     event.preventDefault();
 });
+
+$('#step5').submit(function(event){
+    if ($("#userp1").val() == $("#p1").val() && $("#userp2").val() == $("#p2").val() && $("#userp3").val() == $("#p3").val()) {
+        $("span").text("Good job!").show();
+        return;
+    }
+
+    $("span").text("WRONG!").show().fadeOut(3000)
+    event.preventDefault();
+});
