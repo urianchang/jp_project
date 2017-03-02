f63c8f227bc94c639b73fd1a8002bf07a501ab57
Arnold Son <hyomin.son@alum.utoronto.ca>
2017-03-01 21:22:34 -0800

    merged and added all templates

--- apps/jpskip/static/jpskip/scripts/script.js
+++ apps/jpskip/static/jpskip/scripts/script.js
@@ -1,10 +1,20 @@
-$('#test').submit(function(event){
-    if ($("#guess").val() == $("#answer").val()) {
-        $("span").text("Good job!").show();
+$('#step1').submit(function(event){
+    if ($("#userp1").val() == $("#p1").val()) {
+        $("#msg").text("Good job!").show();
         return;
     }
 
-    $("span").text("WRONG!").show().fadeOut(3000)
+    $("#msg").text("WRONG!").show().fadeOut(3000)
+    event.preventDefault();
+});
+
+$('#step2').submit(function(event){
+    if ($("#userp1").val() == $("#p1").val() && $("#userp2").val() == $("#p2").val()) {
+        $("#msg").text("Good job!").show();
+        return;
+    }
+
+    $("#msg").text("WRONG!").show().fadeOut(3000)
     event.preventDefault();
 });
 
@@ -15,5 +25,15 @@ $('#step5').submit(function(event){
     }
 
     $("span").text("WRONG!").show().fadeOut(3000)
+    event.preventDefault();
+});
+
+$('#step3').submit(function(event){
+    if ($("#userp1").val() == $("#p1").val() && $("#userp2").val() == $("#p2").val() && $("#userp3").val() == $("#p3").val()) {
+        $("#msg").text("Good job!").show();
+        return;
+    }
+
+    $("#msg").text("WRONG!").show().fadeOut(3000)
     event.preventDefault();
 });
--- apps/jpskip/urls.py
+++ apps/jpskip/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^process$', views.process),
+    url(r'^step1$', views.step1),
+    url(r'^step2$', views.step2),
+    url(r'^step3$', views.step3),
     url(r'^step4$', views.step4),
     url(r'^step5$', views.step5),
 ]
